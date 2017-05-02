b3af8148a7a1fba7b4f29a091a67895325fb8f58
urgenmagger <evgen.magger@gmail.com>
2017-05-02 19:05:09 +0300

    JavaDoc

--- chapter_001/src/main/java/ru/job4j/Calculate.java
+++ chapter_001/src/main/java/ru/job4j/Calculate.java
@@ -1,5 +1,14 @@
 package ru.job4j;
+/**
+ *Класс для вывода текста 'Hello World'
+ *@autor Megger
+ *@verson 01
+ */
 class Calculate{
+  /**
+   *method output
+   *@autor Megger
+   */
     public static void main(String[] args) {
         System.out.println("Hello World!");
     }
---
+++ chapter_001/src/main/java/ru/job4j/package-info.java
@@ -0,0 +1,7 @@
+/**
+ *Package for calculate task.
+ *@author Megger 
+ *@version $Id$
+ *@since 0.1
+ * /
+package ru.job4j;
