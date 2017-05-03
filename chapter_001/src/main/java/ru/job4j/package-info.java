ea525a6ddb01bbaccf31bcdd0679e6b6f11f5463
urgenmagger <evgen.magger@gmail.com>
2017-05-03 08:39:11 +0300

    JUnit

--- chapter_001/src/main/java/ru/job4j/package-info.java
+++ chapter_001/src/main/java/ru/job4j/package-info.java
@@ -1,7 +1,8 @@
 /**
- *Package for calculate task.
- *@author Megger 
- *@version $Id$
- *@since 0.1
- * /
+* Package for calculate task.
+*
+* @author Megger
+* @version $Id$
+* @since 0.1
+*/
 package ru.job4j;
