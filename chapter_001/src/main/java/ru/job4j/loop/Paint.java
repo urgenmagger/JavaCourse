5ab9ef8e627724143e378affc4598cbd325ed136
urgenmagger <evgen.magger@gmail.com>
2017-06-02 06:19:31 +0300

    pyramid_01

---
+++ chapter_001/src/main/java/ru/job4j/loop/Paint.java
@@ -0,0 +1,41 @@
+package ru.job4j.loop;
+/**
+ *Drawing a pyramid.
+ *@author Megger
+ *@version $Id$
+ *@since 0.1
+ */
+public class Paint {
+/**
+ *@param h - height of the pyramid.
+ *@return a pyramid in a pseudograph.
+ */
+  public String pyramid(int h) {
+    StringBuilder sb = new StringBuilder();
+   // String newLine = System.getProperty("line.separator");
+    int w = (h + h) - 1;
+    for (int i = 1; i <= h; i++) {
+      for (int j = 1; j <= w; j++) {
+        //draw the left side
+          if ((j + i) <= h && j < h) {
+             sb.append(" ");
+          } else if ((j + i) > h && j < h) {
+             sb.append("^");
+          }
+        //draw the right side
+          if ((j - i) >= h && j > h) {
+             sb.append(" ");
+          } else if ((j - i) < h && j > h) {
+             sb.append("^");
+          }
+          //draw the center
+           if (j == h) {
+             sb.append("^");
+          }
+      }
+          // sb.append(newLine);
+           sb.append(System.getProperty("line.separator"));
+    }
+   return sb.toString();
+  }
+}
---
+++ chapter_001/src/test/java/ru/job4j/loop/PaintTest.java
@@ -0,0 +1,35 @@
+package ru.job4j.loop;
+
+import org.junit.Test;
+import static org.hamcrest.core.Is.is;
+import static org.junit.Assert.assertThat;
+/**
+ *pyramid test.
+ *@author Megger
+ *@version $Id$
+ *@since 0.1
+ */
+
+public class PaintTest {
+    /**
+     *Test.
+     *@pyramid height 1.
+     */
+    @Test
+    public void whenPiramidWithHeightTwoThenStringWithTwoRows() {
+        Paint paint = new Paint();
+        String result = paint.pyramid(2);
+        String expected = String.format(" ^ %s^^^", System.getProperty("line.separator"));
+        assertThat(result, is(expected));
+    }
+    /**
+     *Test.
+     *@pyramid height 3.
+     */
+    @Test
+    public void whenPiramidWithHeightThreeThenStringWithThreeRows() {
+       //напишите здесь тест, проверяющий формирование пирамиды для высоты 3.
+    }
+}
+
+
