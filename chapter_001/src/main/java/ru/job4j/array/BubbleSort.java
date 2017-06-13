6778868ddffa5840d3f330175790ea968f5ba7ac
urgenmagger <evgen.magger@gmail.com>
2017-06-13 12:53:55 +0300

    refactoring sort 01

--- chapter_001/src/main/java/ru/job4j/array/BubbleSort.java
+++ chapter_001/src/main/java/ru/job4j/array/BubbleSort.java
@@ -11,19 +11,16 @@ public class BubbleSort {
  *@return sort array.
  */
   public int[] sort(int[] array) {
-    boolean flag = false;
-    int temp;
-    while (!flag) {
-      flag = true;
-      for (int i = 0; i < array.length - 1; i++) {
-        if (array[i] > array[i + 1]) {
-          temp = array[i];
-          array[i] = array[i + 1];
-          array[i + 1] = temp;
-          flag = false;
+     int temp;
+      for (int i = array.length - 1; i >= 0; i--) {
+        for (int j = 0; j < i; j++) {
+          if (array[j] > array[j + 1]) {
+            temp = array[j];
+            array[j] = array[j + 1];
+            array[j + 1] = temp;
+          }
         }
       }
-    }
     return array;
-  }
-}
+    }
+ }
