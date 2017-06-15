d5072cde283c1770f21db85004fe39ccc24b7f43
urgenmagger <evgen.magger@gmail.com>
2017-06-15 15:56:17 +0300

    refactoring pyramid 01

--- chapter_001/src/main/java/ru/job4j/loop/Paint.java
+++ chapter_001/src/main/java/ru/job4j/loop/Paint.java
@@ -13,23 +13,12 @@ public class Paint {
   public String pyramid(int h) {
     StringBuilder sb = new StringBuilder();
     int w = (h + h) - 1;
-    for (int i = 1; i <= h; i++) {
+    for (int i = 0; i < h; i++) {
       for (int j = 1; j <= w; j++) {
-        //draw the left side
-          if ((j + i) <= h && j < h) {
+          if (j >= h - i && j <= h + i) {
+             sb.append("^");
+          } else {
              sb.append(" ");
-          } else if ((j + i) > h && j < h) {
-             sb.append("^");
-          }
-        //draw the right side
-          if ((j - i) >= h && j > h) {
-             sb.append(" ");
-          } else if ((j - i) < h && j > h) {
-             sb.append("^");
-          }
-          //draw the center
-           if (j == h) {
-             sb.append("^");
           }
       }
            sb.append(System.getProperty("line.separator"));
