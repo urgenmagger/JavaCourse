9c56535f2107ca39202ef3c2cc104c1067877ce2
urgenmagger <evgen.magger@gmail.com>
2018-01-05 17:55:43 +0300

    problem in showAllItems

--- chapter_002/src/main/java/ru/job4j/tracker/StartUI.java
+++ chapter_002/src/main/java/ru/job4j/tracker/StartUI.java
@@ -8,11 +8,11 @@ public class StartUI {
     /**
      * Константа меню для добавления новой заявки.
      */
-    private static final String  finndAll= "1";
+    private static final String findAll = "1";
     /**
      * Константа меню для добавления новой заявки.
      */
-    private static final String  replaseItem = "2";
+    private static final String replaceItem = "2";
     /**
      * Константа меню для добавления новой заявки.
      */
@@ -20,11 +20,11 @@ public class StartUI {
     /**
      * Константа меню для добавления новой заявки.
      */
-    private static final String  findById = "4";
+    private static final String findById = "4";
     /**
      * Константа меню для добавления новой заявки.
      */
-    private static final String  findByName = "5";
+    private static final String findByName = "5";
 
     /**
      * Константа для выхода из цикла.
@@ -42,7 +42,8 @@ public class StartUI {
 
     /**
      * Конструтор инициализирующий поля.
-     * @param input ввод данных.
+     *
+     * @param input   ввод данных.
      * @param tracker хранилище заявок.
      */
     public StartUI(Input input, Tracker tracker) {
@@ -61,8 +62,8 @@ public class StartUI {
             if (ADD.equals(answer)) {
                 //добавление заявки вынесено в отдельный метод.
                 this.createItem();
-//            } else if (...) {
-//             Добавить остальные действия системы по меню.
+            } else if (findAll.equals(answer)) {
+                this.ShowAllItems();
             } else if (EXIT.equals(answer)) {
                 exit = true;
             }
@@ -81,22 +82,34 @@ public class StartUI {
         System.out.println("------------ Новая заявка с getId : " + item.getId() + "-----------");
     }
 
+    /**
+     * Метод реализует показ всех заявок.
+     */
+    private void ShowAllItems() {
+        System.out.println("------------ Все заявки--------------");
+        for (int i = 0; i < tracker.findAll().length; i++) {
+            System.out.println("Имя заявки: " + tracker.findAll()[i].getName());
+            System.out.println("Описание заявки: " + tracker.findAll()[i].getDescription());
+        }
+    }
+
 
     private void showMenu() {
-        System.out.println("Меню."  );
-        System.out.println("0. Add new Item"  );
-        System.out.println("1. Show all items"  );
-        System.out.println("2. Edit item"  );
-        System.out.println("3. Delete item"  );
-        System.out.println("4. Find item by Id"  );
-        System.out.println("5. Find items by name"  );
-        System.out.println("6. Exit Program"  );
-        System.out.println("Select: "  );
+        System.out.println("Меню.");
+        System.out.println("0. Add new Item");
+        System.out.println("1. Show all items");
+        System.out.println("2. Edit item");
+        System.out.println("3. Delete item");
+        System.out.println("4. Find item by Id");
+        System.out.println("5. Find items by name");
+        System.out.println("6. Exit Program");
+        System.out.println("Select: ");
         // добавить остальные пункты меню.
     }
 
     /**
      * Запускт программы.
+     *
      * @param args
      */
     public static void main(String[] args) {
