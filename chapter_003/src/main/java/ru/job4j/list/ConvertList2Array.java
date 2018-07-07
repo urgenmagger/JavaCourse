c001ef624b684b876bb123add596429430477c68
urgenmagger <evgen.magger@gmail.com>
2018-07-07 14:39:07 +0300

    convert list<arrays> to list<Integer>

--- chapter_003/src/main/java/ru/job4j/list/ConvertList2Array.java
+++ chapter_003/src/main/java/ru/job4j/list/ConvertList2Array.java
@@ -1,5 +1,6 @@
 package ru.job4j.list;
 
+import java.util.ArrayList;
 import java.util.List;
 
 public class ConvertList2Array {
@@ -23,6 +24,16 @@ public class ConvertList2Array {
             }
         }
         return array;
+    }
+
+    public List<Integer> convert(List<int[]> list) {
+        List<Integer> result = new ArrayList<>();
+        for (int[] i : list) {
+            for (int j : i) {
+                result.add(j);
+            }
+        }
+        return result;
     }
 }
 
--- chapter_003/src/test/java/ru/job4j/list/ConvertList2ArrayTest.java
+++ chapter_003/src/test/java/ru/job4j/list/ConvertList2ArrayTest.java
@@ -2,7 +2,9 @@ package ru.job4j.list;
 
 import org.junit.Test;
 
+import java.util.ArrayList;
 import java.util.Arrays;
+import java.util.List;
 
 import static org.hamcrest.core.Is.is;
 import static org.junit.Assert.assertThat;
@@ -51,6 +53,25 @@ public class ConvertList2ArrayTest {
                 {7, 8, 9},
                 {10, 0, 0}
         };
+        assertThat(result, is(expect));
+    }
+
+    @Test
+    public void convert2ArrayToList() {
+        List<int[]> list = new ArrayList<>();
+        list.add(new int[]{1, 2});
+        list.add(new int[]{3, 4});
+        list.add(new int[]{3, 4, 5});
+        ConvertList2Array temp = new ConvertList2Array();
+        List<Integer> result = temp.convert(list);
+        List<Integer> expect = new ArrayList<>();
+        expect.add(1);
+        expect.add(2);
+        expect.add(3);
+        expect.add(4);
+        expect.add(3);
+        expect.add(4);
+        expect.add(5);
         assertThat(result, is(expect));
     }
 }
